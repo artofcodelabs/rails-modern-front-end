b902c756d3efa17ae9d6f21588c5a8fa071bdf15
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2018-04-14 22:46:40 +0200

    Rails 5.2

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
+require "active_storage/engine"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
@@ -19,11 +20,12 @@ Bundler.require(*Rails.groups)
 module EvilChat
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     config.generators do |g|
       g.test_framework  false
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,10 +3,13 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
+# Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
-# Pre-compile additional assets.
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile << proc do |path|
   true if path =~ /\.(eot|svg|ttf|woff|png|map)\z/
 end
